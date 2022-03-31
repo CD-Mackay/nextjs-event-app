c2002fb015c216f1ea5da6abcfe095f0f1aaaa48
Connor Mackay <connor.mackay@gmail.com>
2022-03-31 13:06:32 -0400

    build eventssearch component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,12 @@
 import EventList from "../../components/events/eventList";
+import EventsSearch from "../../components/events/eventsSearch";
 import { getAllEvents } from "../../data/dummy-data";
 
 function EventsPage() {
   const events = getAllEvents();
   return (
     <div>
+      <EventsSearch />
       <EventList events={events} />
     </div>
   );
