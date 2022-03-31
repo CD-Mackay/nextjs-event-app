d227f5b85a54d473aaf2a337e40a1c22e2125c3c
Connor Mackay <connor.mackay@gmail.com>
2022-03-31 12:27:34 -0400

    import zipped components

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,19 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../data/dummy-data';
+
 function SingleEventPage() {
+  const router = useRouter();
+
+  router.query;
+
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return <p>No Event Found</p>
+  }
+
   return (
     <div>
       <h1>I am SingleEventPage</h1>
