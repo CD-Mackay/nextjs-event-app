0705e62237ec5d957461b8633034d589a4cc170d
Connor Mackay <connor.mackay@gmail.com>
2022-03-31 12:34:12 -0400

    Add icons

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,9 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../data/dummy-data';
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from "../../data/dummy-data";
+import EventSummary from "../../components/event-details/event-summary";
+import EventLogistics from "../../components/event-details/event-logistics";
+import EventContent from "../../components/event-details/event-content";
 
 function SingleEventPage() {
   const router = useRouter();
@@ -8,17 +12,26 @@ function SingleEventPage() {
 
   const eventId = router.query.eventId;
 
-  const event = getEventById(eventId)
+  const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event Found</p>
+    return <p>No Event Found</p>;
   }
 
   return (
-    <div>
-      <h1>I am SingleEventPage</h1>
-    </div>
-  )
-};
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
 
 export default SingleEventPage;
