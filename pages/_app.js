dc7affec0b4ccef1c5fdf43e7f614a58cb67999d
Connor Mackay <connor.mackay@gmail.com>
2022-04-13 09:37:49 -0400

    Add head to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,9 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
